2bf64c6e5281e291be69ab7e0dd27ac711950ea0
kcaldon <kevincaldon@beachhousecenter.com>
2022-07-18 16:48:16 -0400

    updates 7/18/2022

--- BH_DJANGO/pages/views.py
+++ BH_DJANGO/pages/views.py
@@ -7,3 +7,11 @@ def homepage_view(request, *args, **kwargs):
 
 def viv_view(request, *args, **kwargs):
     return render(request, "viv.html", {})
+
+"""if request.method == 'POST' and 'submit' in request.POST:
+    
+    from vivitrol_script import import_csv_data
+    
+    import_csv_data()
+    
+    return HttpResponseRedirect(reverse(app_name:view_name))"""
