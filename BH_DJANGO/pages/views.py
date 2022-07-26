581a504a235b19b93cd5520e596b840fa0ccec9f
kcaldon <kevincaldon@beachhousecenter.com>
2022-07-26 16:42:39 -0400

    updates 7/26/2022

--- BH_DJANGO/pages/views.py
+++ BH_DJANGO/pages/views.py
@@ -9,6 +9,10 @@ from datetime import datetime, timedelta
 from .forms import Flash_File_Form
 import json
 from django.templatetags.static import static
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+from .dum_graph_func import return_graph
+
 
 def logout_user(request):
     auth.logout(request)
@@ -29,7 +33,8 @@ def homepage_view(request, *args, **kwargs):
         df[['DOC', 'trash.1']] = df.diagcodename_list.str.split(' ', n=1, expand=True)
         df['LNF3'] = df['last_name'].str.slice(stop=3)
         df['Name'] = df.loc[:, 'first_name'] + ' ' + df.loc[:, 'LNF3']
-        df = df[['Name', 'mr', 'admission_date', 'program_name', 'length_of_stay', 'age', 'sex', 'DOC', 'Therapist','paymentmethod']]
+        df = df[['Name', 'mr', 'admission_date', 'program_name', 'length_of_stay', 'age', 'sex', 'DOC', 'Therapist',
+                 'paymentmethod']]
         df['Therapist'] = df['Therapist'].replace(['Did'], 'No Assigned Therapist')
         df['program_name'] = df['program_name'].replace(['2 Detox'], 'DTX')
         df['program_name'] = df['program_name'].replace(['4 Residential'], 'RES')
@@ -39,17 +44,21 @@ def homepage_view(request, *args, **kwargs):
         df.loc[:, ('admission_date_1')] = pandas.to_datetime(df.loc[:, ('admission_date_1')])
         new_admissions = df[(df['admission_date_1'] >= t)].sort_values(by='admission_date_1', ascending=False)
         new_admissions[['admission_date', 'trash.2']] = df.admission_date.str.split(' ', n=1, expand=True)
-        new_admissions[['Y', 'M','D']] = new_admissions.admission_date.str.split('-', n=2, expand=True)
-        new_admissions['admission_date'] = new_admissions.loc[:,('M')] + "-" + new_admissions.loc[:,('D')] + "-" + new_admissions.loc[:,('Y')]
+        new_admissions[['Y', 'M', 'D']] = new_admissions.admission_date.str.split('-', n=2, expand=True)
+        new_admissions['admission_date'] = new_admissions.loc[:, ('M')] + "-" + new_admissions.loc[:,
+                                                                                ('D')] + "-" + new_admissions.loc[:,
+                                                                                               ('Y')]
         new_admissions = new_admissions[
             ['Name', 'mr', 'admission_date', 'program_name', 'length_of_stay', 'age', 'sex', 'DOC', 'Therapist',
              'paymentmethod']]
         json_records = new_admissions.reset_index().to_json(orient='records')
-        data = []
+        # data = []
         data = json.loads(json_records)
         context = {'d': data}
+        # context = {'d': data, 'chart': chart}
+        context['chart'] = return_graph()
 
-
+        #context = {'d': data, 'chart': chart}
 
     return render(request, "home.html", context)
 
@@ -115,13 +124,21 @@ def flash_view(request, *args, **kwargs):
             DL_Flash['Name_3'] = DL_Flash.loc[:, 'First Name'] + ' ' + DL_Flash.loc[:, 'LNF3']
             DL_Flash['Name_2'] = DL_Flash.loc[:, 'First Name'] + ' ' + DL_Flash.loc[:, 'LNF2']
             DL_Flash['Name'] = DL_Flash.loc[:, 'First Name'] + ' ' + DL_Flash.loc[:, 'Last Name']
-            DL_Flash = DL_Flash[['Name', 'MR', 'Sex', 'Insurance 1   Insurance Company', 'Admission Date', 'Length Of Stay', 'Program', 'Payment Method', 'LNF3', 'LNF2', 'Name_3', 'Name_2']]
-            M_D = pandas.merge(DL_Flash, Actual_Flash, how="left", left_on=["Name_3"], right_on=["Patient"], suffixes=("", ".Flash"))
-            M_D = pandas.merge(M_D, Actual_Flash, how="left", left_on=["Name_2"], right_on=["Patient"], suffixes=("", ".Match2"))
-            M_D = pandas.merge(M_D, Actual_Flash, how="left", left_on=["MR"], right_on=["MRN"], suffixes=("", ".MRN_MATCH"))
-            M_D['Boolean'] = numpy.where((M_D['Patient'].isnull()) & (M_D['Patient.Match2'].isnull()) & (M_D['Patient.MRN_MATCH'].isnull()), True, False)
+            DL_Flash = DL_Flash[
+                ['Name', 'MR', 'Sex', 'Insurance 1   Insurance Company', 'Admission Date', 'Length Of Stay', 'Program',
+                 'Payment Method', 'LNF3', 'LNF2', 'Name_3', 'Name_2']]
+            M_D = pandas.merge(DL_Flash, Actual_Flash, how="left", left_on=["Name_3"], right_on=["Patient"],
+                               suffixes=("", ".Flash"))
+            M_D = pandas.merge(M_D, Actual_Flash, how="left", left_on=["Name_2"], right_on=["Patient"],
+                               suffixes=("", ".Match2"))
+            M_D = pandas.merge(M_D, Actual_Flash, how="left", left_on=["MR"], right_on=["MRN"],
+                               suffixes=("", ".MRN_MATCH"))
+            M_D['Boolean'] = numpy.where(
+                (M_D['Patient'].isnull()) & (M_D['Patient.Match2'].isnull()) & (M_D['Patient.MRN_MATCH'].isnull()),
+                True, False)
             M_D = M_D[M_D['Boolean'] == True]
-            M_D = M_D.rename(columns={'Insurance 1   Insurance Company': 'Insurance', 'Admission Date': 'Admission', 'Length Of Stay': 'LOS', 'Payment Method': 'Payment'})
+            M_D = M_D.rename(columns={'Insurance 1   Insurance Company': 'Insurance', 'Admission Date': 'Admission',
+                                      'Length Of Stay': 'LOS', 'Payment Method': 'Payment'})
             M_D = M_D[['Name', 'MR', 'Sex', 'Insurance', 'Admission', 'LOS', 'Program', 'Payment']]
             json_records = M_D.reset_index().to_json(orient='records')
             data = []
