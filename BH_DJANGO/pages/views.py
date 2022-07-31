a61def373304cfbcfbb3bcee7858d88aa466815a
keberelf058725 <kcaldonjr@gmail.com>
2022-07-30 21:09:40 -0400

    Updates 7/29/2022

--- BH_DJANGO/pages/views.py
+++ BH_DJANGO/pages/views.py
@@ -26,6 +26,43 @@ def viv_help_view(request, *args, **kwargs):
 
 @login_required
 def homepage_view(request, *args, **kwargs):
+
+    if request.method == 'POST':
+
+
+        response = HttpResponse(
+            content_type='text/csv',
+            headers={'Content-Disposition': 'attachment; filename="recentadmits.csv"'},
+        )
+
+        t = datetime.today() - timedelta(days=3)
+        df = pandas.read_csv(static('census_info_beachhouse.csv'))
+        df[['Therapist', 'trash']] = df.primarycareteam_primarytherapist.str.split(' ', n=1, expand=True)
+        df[['DOC', 'trash.1']] = df.diagcodename_list.str.split(' ', n=1, expand=True)
+        df['LNF3'] = df['last_name'].str.slice(stop=3)
+        df['Name'] = df.loc[:, 'first_name'] + ' ' + df.loc[:, 'LNF3']
+        df = df[['Name', 'mr', 'admission_date', 'program_name', 'length_of_stay', 'age', 'sex', 'DOC', 'Therapist',
+                 'paymentmethod']]
+        df['Therapist'] = df['Therapist'].replace(['Did'], 'No Assigned Therapist')
+        df['program_name'] = df['program_name'].replace(['2 Detox'], 'DTX')
+        df['program_name'] = df['program_name'].replace(['4 Residential'], 'RES')
+        df['program_name'] = df['program_name'].replace(['5 PHP'], 'PHP')
+        df['program_name'] = df['program_name'].replace(['6 IOP 5 Days'], 'IOP')
+        df['admission_date_1'] = df['admission_date']
+        df.loc[:, ('admission_date_1')] = pandas.to_datetime(df.loc[:, ('admission_date_1')])
+        new_admissions = df[(df['admission_date_1'] >= t)].sort_values(by='admission_date_1', ascending=False)
+        new_admissions[['admission_date', 'trash.2']] = df.admission_date.str.split(' ', n=1, expand=True)
+        new_admissions[['Y', 'M', 'D']] = new_admissions.admission_date.str.split('-', n=2, expand=True)
+        new_admissions['admission_date'] = new_admissions.loc[:, ('M')] + "-" + new_admissions.loc[:,
+                                                                                ('D')] + "-" + new_admissions.loc[:,
+                                                                                               ('Y')]
+        new_admissions = new_admissions[
+            ['Name', 'mr', 'admission_date', 'program_name', 'length_of_stay', 'age', 'sex', 'DOC', 'Therapist',
+             'paymentmethod']]
+        new_admissions.to_csv(path_or_buf=response, float_format='%.2f', index=False, decimal=".")
+
+        return response
+
     if request.method == 'GET':
         t = datetime.today() - timedelta(days=3)
         df = pandas.read_csv(static('census_info_beachhouse.csv'))
