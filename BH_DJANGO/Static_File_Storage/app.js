0b9111dabab7c578ef2cafaca389ed892e7e94e9
kcaldon <kevincaldon@beachhousecenter.com>
2022-08-25 14:16:37 -0400

    re-did landing page and added additional features

--- BH_DJANGO/Static_File_Storage/app.js
+++ BH_DJANGO/Static_File_Storage/app.js
@@ -1,2 +1,13 @@
-gsap.fromTo("#filter_buttons", .5, {x: "-400"}, {x: '0%', ease: Power2.easeInOut});
-gsap.fromTo("#context", .5, {x: "400"}, {x: '0%', ease: Power2.easeInOut});
+
+gsap.fromTo("#context", 1, {x: "400"}, {x: '0%', ease: Power2.easeInOut});
+
+
+const tl1 = new TimelineMax();
+
+tl1.fromTo("#filter_buttons1", .5, {x: "-400"}, {x: '0%', ease: Power2.easeInOut}
+).fromTo("#filter_buttons2", .5, {x: "-400"}, {x: '0%', ease: Power2.easeInOut}
+).fromTo("#filter_buttons3", .5, {x: "-400"}, {x: '0%', ease: Power2.easeInOut}
+).fromTo("#filter_buttons4", .5, {x: "-400"}, {x: '0%', ease: Power2.easeInOut}
+).fromTo("#filter_buttons5", .5, {x: "-400"}, {x: '0%', ease: Power2.easeInOut}
+).fromTo("#filter_buttons6", .5, {x: "-400"}, {x: '0%', ease: Power2.easeInOut}
+).fromTo("#context_home", 1, {opacity: 0}, {opacity: 1, duration: 2});
